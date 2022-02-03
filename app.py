bfe8a84f7ddd7f4e2eccc73a69c27bd68271915e
Pedro Lopez <plopez@poweranalytics.com>
2022-02-03 14:10:16 -0300

    Add Score

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ import pygame, sys, random
 
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, player_score, opponent_score
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
+    if ball.left <= 0:
+        player_score += 1
+        ball_restart()
+
+    if ball.right >= screen_width:
+        opponent_score += 1
         ball_restart()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
@@ -64,6 +69,12 @@ ball_speed_y = 7 * random.choice((1, -1))
 
 player_speed = 0
 opponent_speed = 7
+
+# Text Variables
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 32)
+
 while True:
     # Handling input
     for event in pygame.event.get():
@@ -90,11 +101,14 @@ while True:
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
-
     pygame.draw.aaline(
         screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height)
     )
 
+    player_text = game_font.render(f"{player_score}", False, light_grey)
+    screen.blit(player_text, (660, 470))
+    opponent_text = game_font.render(f"{opponent_score}", False, light_grey)
+    screen.blit(opponent_text, (600, 470))
     # Updating the window
     pygame.display.flip()
     clock.tick(60)
