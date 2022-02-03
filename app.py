10c77b10268171a815e4707ed32306006d69fe10
Pedro Lopez <plopez@poweranalytics.com>
2022-02-03 13:53:11 -0300

    Random movement

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import pygame, sys
-
-ball_speed_x = 7
-ball_speed_y = 7
+import pygame, sys, random
 
 
 def ball_animation():
@@ -12,7 +9,7 @@ def ball_animation():
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
+        ball_restart()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
@@ -37,6 +34,13 @@ def opponent_ai():
         opponent.bottom = screen_height
 
 
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width / 2, screen_height / 2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
+
+
 # General Setup
 pygame.init()
 clock = pygame.time.Clock()
@@ -54,6 +58,9 @@ opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
 
 bg_color = pygame.Color("grey12")
 light_grey = (200, 200, 200)
+
+ball_speed_x = 7 * random.choice((1, -1))
+ball_speed_y = 7 * random.choice((1, -1))
 
 player_speed = 0
 opponent_speed = 7
